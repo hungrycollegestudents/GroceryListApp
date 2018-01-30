6cd24aaa4e0aa41f8ad89025f2c3c64ca8de5cab
jacobbrunson <jacob@brunson.me>
2018-01-30 16:57:21 -0600

    added API classes

---
+++ app/src/main/java/me/brunson/testapplication/API.java
@@ -0,0 +1,13 @@
+package me.brunson.testapplication;
+
+/**
+ * Created by jacob on 1/30/18.
+ */
+
+public class API {
+
+    public API() {
+        System.out.println("hello API!");
+    }
+
+}
---
+++ app/src/main/java/me/brunson/testapplication/WalmartAPI.java
@@ -0,0 +1,9 @@
+package me.brunson.testapplication;
+
+/**
+ * Created by jacob on 1/30/18.
+ */
+
+public class WalmartAPI extends API {
+
+}
