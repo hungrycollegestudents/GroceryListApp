8533260742409f887d1438b61935c4bed5d55fb8
jrthacker <35977048+jrthacker@users.noreply.github.com>
2018-02-06 11:06:02 -0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class User {
+    private int userId = -1; // ID placeholder
+    private String userName = "";
+    private ArrayList<Checklist> userLists = new ArrayList<Checklist>(); // array list to hold lists?
+    
+    public User(){};
+    
+    public User (int id, String name){
+        userId = id;
+        userName = name;
+    }
+    
+    public void setUserName(String name){
+        userName = name;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public String getUserName(){
+        return userName;
+    }
+    
+    public void makeChecklist(){
+        Checklist newList = new Checklist(); // create new checklist
+        userLists.add(newList); // add new list to array list
+    }
+    
+    public void deleteChecklist(int index){ // how to pass index
+        userLists.remove(index); // remove one specific list
+    }
+    
+    public void deleteAllChecklist(){
+        userLists.clear(); // remove all lists
+    }
+    
+}
